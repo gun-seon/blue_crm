c44af4301b338750af58ee9af6887b09b5b9ed49
psns <psns0122@naver.com>
2025-09-20 20:47:31 +0900

    fix: 검색 안내문 수정

--- frontend/src/components/layout/header/SearchBar.vue
+++ frontend/src/components/layout/header/SearchBar.vue
@@ -22,7 +22,7 @@
         <input
           v-model="keyword"
           type="text"
-          placeholder="Search or type command..."
+          placeholder="검색어를 입력하세요"
           @keydown.esc="clearSearch"
           class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pl-12 pr-14 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30 xl:w-[430px]"
         />
